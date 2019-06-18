42b931e489ae1cc559dab8ccae4fad7e2e9a8607
Naeeb MD Ridwan <naeeb@Naeebs-MacBook-Pro.local>
2019-06-18 17:51:07 +0600

    first push

---
+++ index-parts/part3.php
@@ -0,0 +1,16 @@
+<!-- LBIF SECTION STARTS HERE-->
+
+
+<div class="n_lbif">
+<p class="n_p1">BLACK SHEEP STUDIOS ARE BACK AT LBIF</p>
+
+<p class="n_p2">Starting May 11th, Black Sheep will be offering classes at the LBI Arts Foundation! Classes will be held every weekend from 5/11 till 6/25.</p>    
+<p class="n_p3">After June 25th, classes will be held every day.</p> 
+
+    <p class="n_p4"><b class="n_p3">Classes offered are:</b> Barre, Mat Pilates, Beach Boot Camp, Kettlebell, RBT, HIIT, Strength & Core, and Cardio Weights.
+
+</p>
+    
+<a href="http://lbifoundation.org/yoga/" target="blank" class="button lightButton inline-block center-item mts">View Class Schedule</a>
+    
+</div>
