fedd08248d64142d94d18f49fc5417b5693b5c07
Fuad Tanvir <omi.fuad33@gmail.com>
2019-06-19 00:43:27 +0600

    We are blacksheep section done

---
+++ index-parts/part2.php
@@ -0,0 +1,11 @@
+<!-- We are blacksheep starts -->
+<div class="o_blacksheep_text_wrap">
+    <p>#JOINTHEFLOCK</p>
+    <h2>WE ARE BLACK SHEEP</h2>
+    <p class="o_video_credit">Video Credit:</p>
+    <p class="o_video_credit_link"><span>OAK LEAF MEDIA</span><span>VINYL MEDIA</span></p>
+</div>
+<div class="o_home_blacksheep_video_wrap">
+    <iframe title="vimeo-player" src="https://player.vimeo.com/video/318545404" width="1360px" height="720px" frameborder="0" allowfullscreen></iframe>
+</div>
+<!-- We are blacksheep ends -->
